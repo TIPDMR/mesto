7e4aee1d205876c58efd32cd2c3e68849be87d0b
Dmitriy Tipsin <tipsin.dm@yandex.ru>
2022-11-14 18:43:57 +0300

    fix: Добавлена форма в модальное окно, исправлен js

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,35 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function (env) {
 
-  let buttonAddModal = document.querySelector('.profile__button_action_edit');
-  let username = document.querySelector('.profile__title');
-  let description = document.querySelector('.profile__description');
+  let buttonOpenModal = document.querySelector('.profile__button_action_edit');
+  let profileTitle = document.querySelector('.profile__title');
+  let profileDescription = document.querySelector('.profile__description');
 
   let modalBlock = document.querySelector('.modal');
-  let buttonClose = modalBlock.querySelector('.modal__button_close');
-  let inputUsername = modalBlock.querySelector('#username');
-  let inputDescription = modalBlock.querySelector('#description');
-  let buttonSave = modalBlock.querySelector('.modal__button_save');
+  let buttonCloseModal = modalBlock.querySelector('.modal__button_close');
 
-  let loadUsernameDescription = function (evn) {
-    inputUsername.value = username.textContent;
-    inputDescription.value = description.textContent
+  let modalFormBlock = document.querySelector('.modal__form');
+  let inputModalTitle = modalFormBlock.querySelector('.modal__input_title');
+  let inputModalDescription = modalFormBlock.querySelector('.modal__input_description');
+
+  let firstLoad = true;
+
+  let loadProfile = function (evn) {
+    inputModalTitle.value = profileTitle.textContent;
+    inputModalDescription.value = profileDescription.textContent
   }
-  let changeUsernameDescription = function (evn) {
-    username.textContent = inputUsername.value;
-    description.textContent = inputDescription.value;
+
+  let saveProfile = function (evn) {
+    evn.preventDefault();
+    profileTitle.innerHTML = inputModalTitle.value;
+    profileDescription.innerHTML = inputModalDescription.value;
     closeModal();
   }
 
   let openModal = function (evn) {
-    loadUsernameDescription();
+    if (firstLoad) {
+      loadProfile();
+      firstLoad = false;
+    }
     document.body.style.overflow = 'hidden';
     modalBlock.classList.add('modal_visible');
   }
@@ -38,9 +46,9 @@ document.addEventListener("DOMContentLoaded", function () {
     closeModal();
   }
 
-  buttonAddModal.addEventListener('click', openModal);
-  buttonClose.addEventListener('click', closeModal);
+  buttonOpenModal.addEventListener('click', openModal);
+  buttonCloseModal.addEventListener('click', closeModal);
   modalBlock.addEventListener('click', closeModalClickOnBackgroud);
-  buttonSave.addEventListener('click', changeUsernameDescription);
+  modalFormBlock.addEventListener('submit', saveProfile);
 
 })
