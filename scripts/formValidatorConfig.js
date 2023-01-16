1b7bfa3ed106a3ec3f202225d85352d1730e2917
Dmitriy Tipsin <tipsin.dm@yandex.ru>
2023-01-16 20:26:02 +0300

    ref:Добавлен и настроен сборщик webpack

--- scripts/formValidatorConfig.js
+++
@@ -1,8 +0,0 @@
-export const formValidationConfig = {
-  formSelector: '.modal__form',
-  inputSelector: '.modal__input',
-  submitButtonSelector: '.modal__button',
-  inactiveButtonClass: 'modal__button_disabled',
-  inputErrorClass: 'modal__input_type_error',
-  errorClass: 'modal__error_visible'
-}
--- src/components/Section.js
+++ src/components/Section.js
@@ -14,5 +14,4 @@ class Section {
   }
 }
 
-
 export default Section
