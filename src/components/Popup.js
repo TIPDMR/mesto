ee6e74374a7b1d0f3bd7e5f3ac06660ac5431146
Dmitriy Tipsin <tipsin.dm@yandex.ru>
2023-01-19 10:05:09 +0300

    fix: Исправлено поведение закрытия popup при нажатии клавиши esc

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -2,6 +2,7 @@ class Popup {
   constructor(popupContainer) {
     this._popupContainer = popupContainer;
     this._button = this._popupContainer.querySelector('.modal__button_action_close');
+    this._handleEscClose = this._handleEscClose.bind(this)
   }
 
   _handleEscClose(evn) {
@@ -10,23 +11,27 @@ class Popup {
     }
   }
 
-  setEventListeners() {
+  _setKeydownEventListener() {
     document.addEventListener('keydown', this._handleEscClose);
+  }
+
+  setEventListeners() {
     this._button.addEventListener('click', () => this.close());
     this._popupContainer.addEventListener('mousedown', (evn) => (evn.target === evn.currentTarget) ? this.close() : null);
   }
 
-  _unsetEventListeners() {
+  _unsetKeydownEventListener() {
     document.removeEventListener('keydown', this._handleEscClose);
   }
 
   open() {
+    this._setKeydownEventListener();
     this._popupContainer.classList.add('modal_visible');
   }
 
   close() {
+    this._unsetKeydownEventListener();
     this._popupContainer.classList.remove('modal_visible');
-    this._unsetEventListeners();
   }
 }
 
